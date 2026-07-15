a1e4d54bc23e4b1c4031c8431f5643df208afe07
vythili6 <yo.vythilingum@mail.utoronto.ca>
2026-07-14 20:25:19 -0400

    initial commit

---
+++ src/main/java/com/yodishtr/smart_city_guide/SmartCityGuideApplication.java
@@ -0,0 +1,13 @@
+package com.yodishtr.smart_city_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCityGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCityGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yodishtr/smart_city_guide/SmartCityGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yodishtr.smart_city_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
